77150a2dbee6e9722794a6a08152dbf9c6986c30
Benedict Asaka <basaka@Benedicts-MacBook-Pro.local>
2022-09-06 23:27:24 -0400

    edit file

--- E-shop/Assign-python/python_assign/que4.py
+++
@@ -1,9 +0,0 @@
-def pascals_tri(n):
-   nrow = [4]
-   y = [2]
-   for n in range(max(n,0)):
-      print(nrow) # the variable should be nrow
-      # parameter in the zip function should be nrow and the variable to be added is y and not q.
-      nrow=[l+4 for l,r in zip(nrow+y, y+nrow)]
-   return n>=1
-pascals_tri(8) # The function call is missing an "s"
